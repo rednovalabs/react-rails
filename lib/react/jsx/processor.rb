3d3b86d515e22b0032915ff6385f42b3a1983de6
Dan Ott <thedanielott@gmail.com>
2016-01-23 12:44:27 -0800

    Refactor: Any transformer can optionally use proc options.
    This started as a means to get the CI tests passing. It resulted in
    moving some things around, which allows for better future-reuse of procs
    as options.

--- lib/react/jsx/processor.rb
+++ lib/react/jsx/processor.rb
@@ -2,11 +2,7 @@ module React
   module JSX
     class Processor
       def self.call(input)
-        if JSX::transform_options.respond_to?(:call)
-          JSX::transform_with_proc_options(input)
-        else
-          JSX::transform(input[:data])
-        end
+        JSX::transform(input[:data], input)
       end
     end
   end
